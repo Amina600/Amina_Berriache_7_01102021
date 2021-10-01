56d8c86c0b292749d6334596916cc32d18006376
Amina600 <aminaharaoui19@gmail.com>
2021-10-01 14:13:14 +0200

    Créer le serveur, installer expressjs, body-parser.

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+//Importer express(permet d'ajouter une série de fonctions appelées middleware)
+const express = require('express');
+//Importer package body-parser pour extraire l'objet JSON.
+const bodyParser = require('body-parser');
+
+
+const app = express();
+//CORS, système de sécurité empêche les requêtes malveillantes d'accéder à des ressources sensibles
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+//Middleware qui transforme le corps de la requete en objet js utilisable
+app.use(bodyParser.json());
+
+
+// export de app et pouvoir y acceder depuis les autres fichiers js
+module.exports = app;
