7ef3aea7e79e1cbf64da131f9734a9041d1eee8f
Amina600 <aminaharaoui19@gmail.com>
2021-10-05 16:31:45 +0200

    integration maquette inscription

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,44 +1,61 @@
 <template>
     <div class="container1">
          <div class="container">
-        <div class="row">
-            <div class="col">
-           
-                <img alt="Logo de Groupomania" src="../assets/icon-left-font.png">
-                <h4>
-                    Bienvenue sur votre réseau social d'entreprise
-                </h4>
-                
-            </div>
-            <div class="col-1"></div>
-            <div class="col">
-                <div class="card">
-                    <form action="">
-                        <div class="form-group">
-                            <input type="email" id="email" class="form-control" placeholder="Adresse e-mail">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" id="password" class="form-control" placeholder="Mot de passe">
-                        </div>
-                        <div type="button" class="btn-connect btn-primary">Se connecter</div>
-                    </form>
-                    <div class="group">
-                        <div class="border-separate"></div>
-                        <div type="button" class="btn-register btn-success">Créer nouveau compte</div>
-                    </div>
+            <div class="row">
+                <div class="col-6">
+            
+                    <img alt="Logo de Groupomania" src="../assets/icon-left-font.png">
+                    <h4>
+                        Bienvenue sur votre réseau social d'entreprise
+                    </h4>
                     
+                </div>
+                <div class="col-1"></div>
+                <div class="col">
+                    <div class="card">
+                        <form action="">
+                            <div class="form-group">
+                                <input type="email" id="email" class="form-control" placeholder="Adresse e-mail">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" id="password" class="form-control" placeholder="Mot de passe">
+                            </div>
+                            <div type="button" class="btn-connect btn-primary">Se connecter</div>
+                        </form>
+                        <div class="group">
+                            <div class="border-separate"></div>
+                            <register :revele="revele" :toggleRegister="toggleRegister"></register>
+                            <div v-on:click="toggleRegister" type="button" class="btn-register btn-success">Créer nouveau compte</div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
-        
     </div>
-    </div>
+
    
 </template>
 
 <script>
+import Register from './Register.vue'
+
 export default {
-  name: 'Login'
+    name: 'Login',
+    data() {
+        return {
+            revele: false
+        };
+    },
+    components: {
+        'register': Register
+    },
+    methods: {
+    toggleRegister: function() {
+      this.revele = !this.revele;
+    }
+  }
+  
 
 }
 </script>
@@ -65,7 +82,7 @@ p{
             margin-top: 15px;
             margin-bottom: 15px;
             input{
-                padding: 7px;
+                padding: 10px;
             }
             ::placeholder{
                 color: rgb(172, 168, 168);
@@ -75,8 +92,8 @@ p{
         }.btn-connect{
             margin-top: 15px;
             margin-bottom: 15px;
-            border-radius: 5px;
-            padding: 7px;
+            border-radius: 3px;
+            padding: 10px;
             font-size: 1.1em;
             font-weight: 600;
             background-color: #fc512f;
@@ -97,8 +114,8 @@ p{
             .btn-register{
                 margin-top: 15px;
                 margin-bottom: 15px;
-                border-radius: 5px;
-                padding: 7px;
+                border-radius: 3px;
+                padding: 10px;
                 font-size: 1.1em;
                 font-weight: 500;
                 width: 300px;
---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="bloc-register" v-if="revele">
+        <div class="overlay" v-on:click="toggleRegister"></div>
+
+        <div class="register card">
+            <div class="header">
+                <div class="title">
+                    <h2>S'inscrire</h2> 
+                </div>
+                <div v-on:click="toggleRegister" class="btn-close btn "></div>
+            </div>
+            
+            <form action="">
+                <div class="form-group">
+                    <input type="text" id="pseudo" class="form-control" placeholder="Pseudo">
+                </div>
+                <div class="form-group">
+                    <input type="email" id="email" class="form-control" placeholder="Adresse e-mail">
+                </div>
+                <div class="form-group">
+                    <input type="password" id="password" class="form-control" placeholder="Mot de passe">
+                </div>
+                <div type="button" class="btn-register btn-primary">S'inscrire</div>
+            </form>
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    name: 'Register',
+    props: ['revele', 'toggleRegister']
+}
+</script>
+
+<style scoped lang="scss">
+.bloc-register{
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .overlay{
+        background: rgba(245, 241, 241, 0.589);
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+    .register{
+        background: #f1f1f1;
+        color: #333;
+        padding: 30px;
+        position: fixed;
+        top: 20%;
+        width: 450px !important;
+        box-shadow: 0px 0px 7px 0px rgb(150, 149, 149);
+        background-color: #fff ;
+        .header {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-start;
+            margin-bottom: 30px;
+            border-bottom: 0.2px solid rgb(212, 208, 208);
+            width: 100%;
+            .btn-close {
+                font-size: 1.5em;
+                color: rgb(212, 208, 208);
+                font-weight: 700;
+                
+            }
+
+        }.form-group{
+            margin-bottom: 20px;
+            input{
+                background-color: rgb(247, 247, 247);
+                padding: 10px;
+            }::placeholder{
+                color: rgb(172, 168, 168);
+                font-size: 1em;
+                font-weight: 600;
+            }
+            
+        }.btn-register{
+            margin-top: 50px;
+            margin-bottom: 25px;
+            border-radius: 3px;
+            padding: 10px;
+            font-size: 1.1em;
+            font-weight: 600;
+            background-color:#ff7e6b;
+            &:hover{
+                background-color: #fc643c;
+            }
+        }
+    }
+
+}
+
+
+
+
+
+</style>
